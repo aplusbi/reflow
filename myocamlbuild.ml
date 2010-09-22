a5e0b3f8ae8fd45d528914623637ab5600ab0473
Niki Yoshiuchi <aplusbi@gmail.com>
2010-09-21 22:51:54 -0400

    Added files for ocamlbuild.  Still incomplete - it won't compile the c file.

---
+++ myocamlbuild.ml
@@ -0,0 +1,15 @@
+open Ocamlbuild_plugin
+open Command
+
+let clibdir = "-L/usr/lib";;
+let clibs = "-lutil";;
+let obj_files = "ocaml_forkpty.o";;
+
+(*let cc = env "%.c" in*)
+  (*Cmd(S[ocamlc; A "-cc"; A "gcc"; T(tags_of_pathname cc); A cc]);;*)
+
+dispatch begin function
+  | After_rules ->
+      flag ["link"; "ocaml"; "byte"] (S[A "-custom"; A obj_files; A "-cclib"; A clibdir; A "-cclib"; A clibs]);
+  | _ -> ()
+end
